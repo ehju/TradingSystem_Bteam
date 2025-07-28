0020afbbb08b1b288a5cc308904b234d5e422cea
ehju <eh13.ju@samsung.com>
2025-07-28 10:01:39 +0900

    create mock object

--- TradingSystem/test.cpp
+++ TradingSystem/test.cpp
@@ -1,6 +1,26 @@
+
 #include "gmock/gmock.h"
 using namespace testing;
 
-TEST(TS1, TC1) {
+TEST(TS1, TC2) {
 	EXPECT_EQ(1, 1);
 }
+
+class MockDriver : public StockBrockerDriver {
+public:
+	MOCK_METHOD(void, login, (std::string id, std::string password), (override));
+	MOCK_METHOD(void, buy, (int code, int price, int amount), (override));
+	MOCK_METHOD(void, sell, (int code, int price, int amount), (override));
+	MOCK_METHOD(int, getPrice, (int code), (override));
+};
+
+TEST(LOGINTEST, TC1) {
+	NiceMock<MockDriver> mock;
+	std::string id = "wrongid";
+	std::string password = "password";
+	AutoTradingSystem app{ &mock };
+	EXPECT_CALL(mock, login(id, password))
+		.Times(1);
+	app.login(id, password);
+}
+
